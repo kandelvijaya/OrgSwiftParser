589e5e6ec2159a2820b1bc6629ac1dd01af4917c
kandelvijaya <kandelvijaya@gmail.com>
2018-03-24 00:32:32 +0100

    Fixed mutual recursion with dictonary lookup by using forward reference

--- ParserCombinator.playground/Contents.swift
+++ ParserCombinator.playground/Contents.swift
@@ -45,38 +45,7 @@ func jNumber() -> Parser<JSONValue> {
 }
 
 
-
-// MARK:- The entire json parser
-func jsonParser() -> Parser<JSONValue> {
-    return [jNull(), jNumber(), jString(), jArray(), jObject()] |> choice
-}
-
-
-
-// MARK:- json Array parser
-func jArray() -> Parser<JSONValue> {
-    let left = pchar("[")
-    let right = pchar("]")
-    let ws = pwhitespace |> many
-    let comma = pchar(",")
-    let value = jsonParser()
-    let wsValue = (ws >>- value ->> ws)
-    let wsValueComma = wsValue ->> comma |> many
-    
-    let parser = left >>- wsValueComma ->>- wsValue ->> right |>> { $0.0 + [$0.1] } |>> { JSONValue.array($0) }
-    
-    return parser <?> "JSON Array"
-}
-
-
 // MARK:- JSON String parser
-
-func jStringQuotedRaw() -> Parser<String> {
-    let quote = pchar("\"") <?> "quote"
-    let jChar = jEscapedChars <|> jUnescapedChar <|> jUnicodeChars
-    let parser = quote >>- jChar |> many ->> quote
-    return parser |>> { String($0) }
-}
 
 let jUnescapedChar = satisfy({ $0 != Character("\\") || $0 != Character("\"")}, label: "char")
 
@@ -120,6 +89,14 @@ var jUnicodeChars: Parser<Character> {
 }
 
 
+func jStringQuotedRaw() -> Parser<String> {
+    let quote = pchar("\"") <?> "quote"
+    let jChar = jEscapedChars <|> jUnescapedChar <|> jUnicodeChars
+    let parser = quote >>- jChar |> many ->> quote
+    return parser |>> { String($0) }
+}
+
+
 func jString() -> Parser<JSONValue> {
     // Main string parser
     var jStringQuoted: Parser<JSONValue> {
@@ -137,15 +114,15 @@ func jObject() -> Parser<JSONValue> {
     let key = jStringQuotedRaw()
     let colon = pchar(":")
     let whiteSpace = whitespacces.map(pchar) |> choice |> many
+    let parser = parserLookup.currentValue()
     
     let wsKey = whiteSpace >>- key ->> whiteSpace
-    let wsValue = whiteSpace >>- jsonParser() ->> whiteSpace
+    let wsValue = whiteSpace >>- parser ->> whiteSpace
     
     let keyValue1OrMany = (wsKey ->> colon) ->>- wsValue |> many1
     
 
     func kvm() -> Parser<[(String, JSONValue)]> {
-        print("asking for kvm")
         return keyValue1OrMany
     }
     //    let objectMatcher = open >>- keyValue1OrMany ->> close
@@ -156,6 +133,58 @@ func jObject() -> Parser<JSONValue> {
         return JSONValue.object(x)
     } <?> "JSON Object"
 }
+
+final class MutualRecursionForwardReference<T> {
+    
+    var recursion_lookup_table: [String: T] = [:]
+    private let key = "Key"
+    
+    init(with initialReference: T) {
+        print("Forward reference initialized with \(initialReference)")
+        recursion_lookup_table[key] = initialReference
+    }
+    
+    func forwardReference(to eventualReference: T) {
+        print("Forward reference evaluated to \(eventualReference)")
+        recursion_lookup_table[key] = eventualReference
+    }
+    
+    func currentValue() -> T {
+        let value = recursion_lookup_table[key]
+        print("forward reference obtined is \(value!)")
+        return value as! T
+    }
+}
+
+var parserLookup: MutualRecursionForwardReference<Parser<JSONValue>>!
+parserLookup = MutualRecursionForwardReference(with: jNull())
+
+// MARK:- The entire json parser
+func jsonParser() -> Parser<JSONValue> {
+    return [jNull(),
+            jBool(),
+            jNumber(),
+            jString(),
+            jArray(),
+            jObject()] |> choice <?> "JSON Object"
+}
+
+// MARK:- json Array parser
+func jArray() -> Parser<JSONValue> {
+    let left = pchar("[")
+    let right = pchar("]")
+    let ws = pwhitespace |> many
+    let comma = pchar(",")
+    
+    let value = parserLookup.currentValue()
+    print("Obtained parser is \(value)")
+    
+    let wsValue = (ws >>- value ->> ws)
+    let wsValueComma = wsValue ->> comma |> many
+    let parser = left >>- wsValueComma ->>- wsValue ->> right |>> { $0.0 + [$0.1] } |>> { JSONValue.array($0) }
+    return parser <?> "JSON Array"
+}
+
 
 /// Addition on Dictionaries. Prefer rhs when duplicate
 func +<T,U>(_ lhs: [T:U], _ rhs: [T:U]) -> [T:U] {
@@ -177,10 +206,8 @@ jNumber() |> run("123")
 jNumber() |> run("-123")
 jNumber() |> run("123.12")
 jNumber() |> run("-123.12")
-let bunchOfArr = "[\"true\", \"true\", \"false\", \"null\", 123.0 ]"
-//let boolsArr = jArr |> run(bunchOfArr) |> show
+let bunchOfArr = "[ \"true\", \"true\", \"false\", \"null\", 123.0 ]"
 
 
-//jObject //|> run(bunchOfArr) |> show
-
-jsonParser()
+parserLookup.forwardReference(to: jsonParser())
+jsonParser() |> run(bunchOfArr) |> show
